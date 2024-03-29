151d81472515c9a67817de49118a48651a3fc87d
Cristian Sulea <cristian.sulea.79@gmail.com>
2024-03-30 01:42:08 +0200

    cristiansulea/lftp:3.19.1-r0

--- docker/lftp-download.sh
+++ docker/lftp-download.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
 docker run --rm \
-           --env REMOTE_SERVER="$2" \
-           --env USERNAME="$3" \
-           --env PASSWORD="$4" \
+           --env REMOTE_SERVER="$1" \
+           --env USERNAME="$2" \
+           --env PASSWORD="$3" \
            --env REMOTE_PATH="/" \
-           --volume "$5":/files \
+           --volume "$4":/files \
            cristiansulea/lftp:3.19.1-r0 /download.sh
